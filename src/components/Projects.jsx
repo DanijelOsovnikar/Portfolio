b0bfc2cc61df376f4b071051944f0eaefc8f41e6
Danijel Osovnikar <danijel.osovnikar@gmail.com>
2024-04-23 21:07:38 +0200

    Updated content

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,6 +4,21 @@ export default function Projects({ onClick }) {
   const clickHandler = () => {
     onClick();
   };
+
+  const projectOpening = (e) => () => {
+    if (e === "thePlanets") {
+      document.getElementById("planetsId").style = {
+        transform: "translateX(0)",
+      };
+      console.log(document.getElementById("planetsId"));
+    }
+    if (e === "swipe") {
+    }
+    if (e === "coffeeRoasters") {
+    }
+    if (e === "github") {
+    }
+  };
   return (
     <div className="projects">
       <div className="projects-section">
@@ -13,6 +28,13 @@ export default function Projects({ onClick }) {
         </div>
         <div className="projects-grid">
           <div className="project">
+            <h2>Work in progress</h2>
+            <video controls>
+              <source src="assets/Screen recording.mp4" type="video/mp4" />
+            </video>
+          </div>
+          <div className="project">
+            <h2>Finished projects</h2>
             <input
               type="checkbox"
               name="showHide1"
@@ -37,7 +59,26 @@ export default function Projects({ onClick }) {
                 </a>
               </div>
             </label>
-            <img src="assets/planetsMobile.png" className="thePlanets" />
+            <img
+              src="assets/planetsMobile.png"
+              className="thePlanets"
+              onClick={projectOpening("thePlanets")}
+            />
+            <div className="infoSection">
+              <h3>The Planets</h3>
+              <a
+                href="https://danijelosovnikar.github.io/The-Planets/"
+                target="_blank"
+              >
+                VIEW PAGE
+              </a>
+              <a
+                href="https://github.com/DanijelOsovnikar/The-Planets"
+                target="_blank"
+              >
+                VIEW SOURCE CODE
+              </a>
+            </div>
           </div>
           <div className="project">
             <input
@@ -64,7 +105,26 @@ export default function Projects({ onClick }) {
                 </a>
               </div>
             </label>
-            <img src="assets/swipeMobile.png" className="swipe" />
+            <img
+              src="assets/swipeMobile.png"
+              className="swipe"
+              onClick={projectOpening("swipe")}
+            />
+            <div className="infoSection">
+              <h3>Swipe</h3>
+              <a
+                href="https://danijelosovnikar.github.io/Swipe/"
+                target="_blank"
+              >
+                VIEW PAGE
+              </a>
+              <a
+                href="https://github.com/DanijelOsovnikar/Swipe"
+                target="_blank"
+              >
+                VIEW SOURCE CODE
+              </a>
+            </div>
           </div>
           <div className="project">
             <input
@@ -91,7 +151,26 @@ export default function Projects({ onClick }) {
                 </a>
               </div>
             </label>
-            <img src="assets/coffeeMobile.png" className="coffeeRoasters" />
+            <img
+              src="assets/coffeeMobile.png"
+              className="coffeeRoasters"
+              onClick={projectOpening("coffeeRoasters")}
+            />
+            <div className="infoSection">
+              <h3>coffeeRoasters</h3>
+              <a
+                href="https://danijelosovnikar.github.io/CoffeeRoasters/"
+                target="_blank"
+              >
+                VIEW PAGE
+              </a>
+              <a
+                href="https://github.com/DanijelOsovnikar/CoffeeRoasters"
+                target="_blank"
+              >
+                VIEW SOURCE CODE
+              </a>
+            </div>
           </div>
           <div className="project">
             <input
@@ -118,7 +197,26 @@ export default function Projects({ onClick }) {
                 </a>
               </div>
             </label>
-            <img src="assets/devMobile.png" className="github" />
+            <img
+              src="assets/devMobile.png"
+              className="github"
+              onClick={projectOpening("github")}
+            />
+            <div className="infoSection">
+              <h3>devFinder</h3>
+              <a
+                href="https://danijelosovnikar.github.io/GitHub-User-Search-App/"
+                target="_blank"
+              >
+                VIEW PAGE
+              </a>
+              <a
+                href="https://github.com/DanijelOsovnikar/GitHub-User-Search-App"
+                target="_blank"
+              >
+                VIEW SOURCE CODE
+              </a>
+            </div>
           </div>
         </div>
       </div>
